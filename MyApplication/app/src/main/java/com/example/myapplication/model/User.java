b89dee168f53175c2971f341988d7dbddd5871c5
Shuhui Yang <u7662582@anu.edu.au>
2024-04-20 15:18:49 +1000

    User model constructed

--- MyApplication/app/src/main/java/com/example/myapplication/model/User.java
+++ MyApplication/app/src/main/java/com/example/myapplication/model/User.java
@@ -1,4 +1,56 @@
 package com.example.myapplication.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    public List<FoodBank> subscribedFoodBanks;
+
+    private String contactNumber;
+
+    private String userName = null;
+
+
+    public final String account;
+
+
+    public User(String account) {
+        this.account=account;
+        subscribedFoodBanks = new ArrayList<>();
+
+    }
+
+    public void setUserName(String name){
+        userName = name;
+    }
+
+    public void setContactNumber(String number){
+        contactNumber = number;
+    }
+
+    public void addSubscribedFoodBank(FoodBank foodBank){
+        subscribedFoodBanks.add(foodBank);
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public List<FoodBank> getSubscribedFoodBanks() {
+        return subscribedFoodBanks;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+
+
+
+
 }
