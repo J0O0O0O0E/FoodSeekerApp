cd5479008ed910bc571121f616d098eea04f6e18
SI CHEN <u7756543@gmail.com>
2024-04-20 12:25:44 +1000

    modify foodbank function

--- MyApplication/app/src/main/java/com/example/myapplication/ui/foodbank/FoodbankFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/foodbank/FoodbankFragment.java
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,10 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentFoodbankBinding;
-import com.example.myapplication.databinding.FragmentHomeBinding;
-import com.example.myapplication.ui.home.HomeViewModel;
 
 public class FoodbankFragment extends Fragment {
 
