55cc6e686beb951467f4c6130d7eca6217c46bc1
Shuhui Yang <u7662582@anu.edu.au>
2024-04-22 15:10:21 +1000

    Set the value of mText to the user's email

--- MyApplication/app/src/main/java/com/example/myapplication/ui/profile/ProfileViewModel.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/profile/ProfileViewModel.java
@@ -4,15 +4,55 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.myapplication.model.User;
+import com.example.myapplication.repository.UserRepository;
+
+import java.util.Objects;
+
 public class ProfileViewModel extends ViewModel {
     private final MutableLiveData<String> mText;
 
+    public static UserRepository userRepository;
+
+    public static User user;
+
+    public static String email;
+
+
+
+
+
     public ProfileViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is profile fragment");
+        userRepository = UserRepository.getInstance();
+        String profileName = userRepository.getUser().getValue().email;
+        mText.setValue(profileName);
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+
+
+
+
+    public void setUserName(String newName){
+        user.setUserName(newName);
+
+    }
+
+    public void setContactNumber(String number){
+        user.setContactNumber(number);
+    }
+
+
+
+
+
+
+//
+//    public LiveData<String> getUserName() {
+//    }
 }
+
+
