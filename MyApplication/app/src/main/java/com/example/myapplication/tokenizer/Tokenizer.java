efaade61e5d1577d4ae71154129979062d59edf2
Zijian Yang <u7724610@anu.edu.au>
2024-05-03 19:41:05 +1000

    Add tokenizer abstract class to standardise the concrete implementation framework.

--- MyApplication/app/src/main/java/com/example/myapplication/tokenizer/Tokenizer.java
+++ MyApplication/app/src/main/java/com/example/myapplication/tokenizer/Tokenizer.java
@@ -3,21 +3,8 @@ package com.example.myapplication.tokenizer;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Tokenizer {
-    private ArrayList<String> tokens = new ArrayList<>();
-
-    public Tokenizer(String input) {
-        tokenize(input);
-    }
-
-    private void tokenize(String input) {
-        StringTokenizer st = new StringTokenizer(input, " ,.;");
-        while (st.hasMoreTokens()) {
-            tokens.add(st.nextToken());
-        }
-    }
-
-    public ArrayList<String> getTokens() {
-        return tokens;
-    }
+public abstract class Tokenizer {
+    public abstract boolean hasNext();
+    public abstract boolean next();
+    public abstract Token current();
 }
