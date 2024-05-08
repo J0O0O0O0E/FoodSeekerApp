6a5b4282ce976082adc6913a13291f33bf60af72
Shuhui Yang <u7662582@anu.edu.au>
2024-05-09 04:43:35 +1000

    A BusinessHours instance added as a field

--- MyApplication/app/src/main/java/com/example/myapplication/model/FoodBank.java
+++ MyApplication/app/src/main/java/com/example/myapplication/model/FoodBank.java
@@ -1,4 +1,7 @@
 package com.example.myapplication.model;
+
+import com.example.myapplication.parser.BusinessHours;
+
 /**
  * Represents a food bank entity with detailed attributes relating to its operation, location,
  * and food inventory. This class manages a food bank's data, including storage capacities,
@@ -43,6 +46,8 @@ public class FoodBank {
     private Location location;
     // Distance to user (meter)
     private double distanceToUser;
+
+    private BusinessHours businessHours;
 
     // Constructor
     public FoodBank() {
@@ -263,5 +268,13 @@ public class FoodBank {
     public void setDistanceToUser(double distanceToUser) {
         this.distanceToUser = distanceToUser;
     }
+
+    public BusinessHours getBusinessHours(){
+        if(businessHours == null){
+            this.businessHours = new BusinessHours();
+        }
+        return businessHours;
+    }
+
 }
 
