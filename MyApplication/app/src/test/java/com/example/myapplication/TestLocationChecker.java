1c0d8fa92d2695a8fb3395125f40b9d50c2a3400
u7747847 <Haoxuan.xu@anu.edu.au>
2024-05-09 02:25:03 +1000

    Test for LocationChecker class. There is something wrong with the algorithm. Cannot pass all the tests.

---
+++ MyApplication/app/src/test/java/com/example/myapplication/TestLocationChecker.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+import com.example.myapplication.utils.LocationChecker;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class TestLocationChecker {
+
+    private int state1,state2, state3, state4, state5, state6, state7 = 0;
+
+
+    @Before
+    public void setUp(){
+        state1 = LocationChecker.determineState(-41.055, 145.903);//TAS
+        state2 = LocationChecker.determineState(-31.946, 115.812);//WA
+        state3 = LocationChecker.determineState(-34.993, 138.511);//SA
+        state4 = LocationChecker.determineState(-33.829, 150.966);//NSW
+        state5 = LocationChecker.determineState(-27.566, 152.936);//QLD
+        state6 = LocationChecker.determineState(-37.76, 144.964);//VIC
+        state7 = LocationChecker.determineState(-12.442, 130.921);//NT
+    }
+
+    @Test
+    public void testDetermineState(){
+        assertEquals(state1, 1);
+        assertEquals(state2, 2);//Error here
+        assertEquals(state3, 3);
+        assertEquals(state4, 4);
+        assertEquals(state5, 5);
+        assertEquals(state6, 6);
+        assertEquals(state7, 7);
+    }
+
+
+}
