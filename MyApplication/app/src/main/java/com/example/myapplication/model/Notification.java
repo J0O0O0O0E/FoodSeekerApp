117751e2f8fd7d3fc6b7def50972a467558a049b
Shuhui Yang <u7662582@anu.edu.au>
2024-05-09 04:43:52 +1000

    The model of notifications

--- MyApplication/app/src/main/java/com/example/myapplication/model/Notification.java
+++ MyApplication/app/src/main/java/com/example/myapplication/model/Notification.java
@@ -1,4 +1,44 @@
 package com.example.myapplication.model;
 
+import java.time.LocalDateTime;
+
 public class Notification {
-}
+    private FoodBank foodBank;
+    private boolean status;
+    private LocalDateTime notificationTime;
+
+    private String foodBankName;
+
+    private String content;
+
+    public Notification(FoodBank foodBank, LocalDateTime notificationTime){
+        this.foodBank = foodBank;
+        this.notificationTime = notificationTime;
+        this.foodBankName = foodBank.getName();
+    }
+
+
+    public LocalDateTime getNotificationTime() {
+        return notificationTime;
+    }
+
+    public String getFoodBankName(){
+        return foodBankName;
+    }
+
+    public boolean ifNotify(){
+        return foodBank.getBusinessHours().ifNotifyNeeded(notificationTime);
+    }
+
+    public String getNotifyMessage(){
+        if (foodBank.getBusinessHours().isFoodBankClosed(notificationTime)) {
+            return foodBank.getName() + " is now closed.";
+        } else {
+            return foodBank.getName() + " is now open.";
+        }
+    }
+
+
+    }
+
+
