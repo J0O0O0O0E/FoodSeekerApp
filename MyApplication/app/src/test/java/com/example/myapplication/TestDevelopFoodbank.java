cfd68afc143dd3518cf8ad24c8cf805fc59b962a
u7747847 <Haoxuan.xu@anu.edu.au>
2024-05-09 03:11:16 +1000

    Test for DF.

---
+++ MyApplication/app/src/test/java/com/example/myapplication/TestDevelopFoodbank.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+import com.example.myapplication.model.FoodBank;
+import com.example.myapplication.utils.DevelopFoodbank;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestDevelopFoodbank {
+
+    private ArrayList<FoodBank> foodbanks = new ArrayList<>(20);
+
+    @Before
+    public void setUp(){
+    }
+}
