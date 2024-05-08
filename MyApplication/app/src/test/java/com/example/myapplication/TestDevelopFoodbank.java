9098cdb6f68d87d0caf6c58abec7c8515d26f75a
u7747847 <Haoxuan.xu@anu.edu.au>
2024-05-09 05:23:30 +1000

    Commit test

---
+++ MyApplication/app/src/test/java/com/example/myapplication/TestDevelopFoodbank.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import com.example.myapplication.model.FoodBank;
+import com.example.myapplication.utils.DevelopFoodbank;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class TestDevelopFoodbank {
+    FoodBank[] foodBanks = new FoodBank[20]; // 创建一个FoodBank数组来存放实例
+    String[] ids = {"JERSEY1", "TARAKAN2", "BULCOCK3", "REGENCY4", "OAKS5", "DALTON6", "FRANKLAND7",
+            "COONGAN8", "STRADBROKE9", "EARL10", "FORESTRY11", "BIRKBECK12", "BRONBERG13", "OAK14",
+            "MURRAY15", "VOLGA16", "BELMORE17", "MARK18", "CENTRAL19", "FLEMING20"};
+    @Before
+    public void setUp(){
+
+    }
+
+
+
+
+}
