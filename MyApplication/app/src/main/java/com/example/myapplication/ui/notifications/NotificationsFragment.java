61aa814b7a80a42d0baf8218134766ef460dfd75
Shuhui Yang <u7662582@anu.edu.au>
2024-05-14 18:12:17 +1000

    Set the adapter in the fragment

--- MyApplication/app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
@@ -13,12 +13,15 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.myapplication.databinding.FragmentNotificationsBinding;
+import com.example.myapplication.model.FoodBank;
 import com.example.myapplication.model.Notification;
 import com.example.myapplication.model.User;
 import com.example.myapplication.parser.BusinessHours;
 import com.example.myapplication.repository.UserRepository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class NotificationsFragment extends Fragment {
@@ -34,23 +37,33 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
-        MutableLiveData<LocalDateTime> timeLiveData = UserRepository.getInstance().getTimeLiveData();
-
-        timeLiveData.observe(requireActivity(), new Observer<LocalDateTime>() {
-            @Override
-            public void onChanged(LocalDateTime time) {
-                if(hours.ifNotifyNeeded(time)){
-                    if(hours.isFoodBankClosed(time)){
-//                        Notification notification = new Notification()
-                    }
-                }
+        List<FoodBank> subscribedFoodBanks = UserRepository.getInstance().getSubscribedFoodBanks();
+        List<Notification> notifications = new ArrayList<>();
+        NotificstionsAdapter adapter = new NotificstionsAdapter(getContext(),notifications);
 
 
-            }
-        });
+
+
+
+
+//
+//        final TextView textView = binding.textNotifications;
+//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//
+//        MutableLiveData<LocalDateTime> timeLiveData = UserRepository.getInstance().getTimeLiveData();
+//
+//        timeLiveData.observe(requireActivity(), new Observer<LocalDateTime>() {
+//            @Override
+//            public void onChanged(LocalDateTime time) {
+//                if(hours.ifNotifyNeeded(time)){
+//                    if(hours.isFoodBankClosed(time)){
+////                        Notification notification = new Notification()
+//                    }
+//                }
+//
+//
+//            }
+//        });
 
 
 
