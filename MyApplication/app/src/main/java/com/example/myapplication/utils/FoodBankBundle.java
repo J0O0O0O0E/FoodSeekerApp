49258086e78b4c3d47245110fdc427017c39615a
SI CHEN <u7756543@gmail.com>
2024-05-15 02:05:37 +1000

    add putString(region) in FoodBankBundle.java

--- MyApplication/app/src/main/java/com/example/myapplication/utils/FoodBankBundle.java
+++ MyApplication/app/src/main/java/com/example/myapplication/utils/FoodBankBundle.java
@@ -26,40 +26,10 @@ public class FoodBankBundle {
     public static final String KEY_PIE = "Pie";
     public static final String KEY_VEGETABLE = "Vegetable";
 
+    public static final String KEY_FOODBANKBUNDLE_REGION = "fb_region";
+
     public static Bundle createFoodBankBundle(FoodBank foodBank) {
         Bundle bundle = new Bundle();
-//        bundle.putString("fb_name", foodBank.getName());
-//        bundle.putString("fb_number", foodBank.getTel());
-//        bundle.putString("fb_email", foodBank.getEmail());
-//
-//        if (foodBank.isStatus()) {
-//            bundle.putString("fb_sate", "open");
-//        } else {
-//            bundle.putString("fb_sate", "close");
-//        }
-//
-//        bundle.putString("fb_street", foodBank.getStreet());
-//        bundle.putString("fb_city", foodBank.getSuburb());
-//        bundle.putString("fb_postCode", foodBank.getPostcode());
-//        bundle.putString("fb_country", foodBank.getCountry());
-//        bundle.putString("fb_openHours", foodBank.getOpen_hours());
-//        bundle.putInt("fb_capacity", foodBank.getCapacity());
-//        bundle.putDouble("fb_distance", foodBank.getDistanceToUser());
-//        bundle.putString("fb_foundDate",foodBank.getDoe());
-//        bundle.putDouble("fb_latitude", foodBank.getLat());
-//        bundle.putDouble("fb_longitude", foodBank.getLon());
-//        bundle.putInt("fb_foodBankId", foodBank.getId());
-//        bundle.putDouble("fb_rate",foodBank.getRating());
-//
-//        // Add food quantities
-//        bundle.putInt("Pasta", foodBank.getFood1_pasta());
-//        bundle.putInt("Bread", foodBank.getFood2_bread());
-//        bundle.putInt("Milk", foodBank.getFood3_milk());
-//        bundle.putInt("Pie", foodBank.getFood4_pie());
-//        bundle.putInt("Vegetable", foodBank.getFood5_vet());
-//        bundle.putDouble("fb_latitude",foodBank.getLat());
-//        bundle.putDouble("fb_longitude",foodBank.getLon());
-//        bundle.putInt("fb_foodBankId",foodBank.getId());
 
         if (foodBank.isStatus()) {
             bundle.putString(KEY_FOODBANKBUNDLE_STATE, "open");
@@ -67,6 +37,7 @@ public class FoodBankBundle {
             bundle.putString(KEY_FOODBANKBUNDLE_STATE, "close");
         }
 
+        bundle.putString(KEY_FOODBANKBUNDLE_REGION,foodBank.getRegion());
         bundle.putString(KEY_FOODBANKBUNDLE_NAME, foodBank.getName());
         bundle.putString(KEY_FOODBANKBUNDLE_NUMBER, foodBank.getTel());
         bundle.putString(KEY_FOODBANKBUNDLE_EMAIL, foodBank.getEmail());
