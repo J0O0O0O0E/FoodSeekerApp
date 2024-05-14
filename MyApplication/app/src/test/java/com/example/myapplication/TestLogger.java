6afad1fb7de5a83513e75931c12f2215db0ce87f
u7747847 <Haoxuan.xu@anu.edu.au>
2024-05-14 18:58:24 +1000

    New activity: show the logo.

--- MyApplication/app/src/test/java/com/example/myapplication/TestLogger.java
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication;
-import com.example.myapplication.model.Logger;
-import org.junit.Before;
-import org.junit.Test;
-import java.util.ArrayList;
-import static org.junit.Assert.*;
-public class TestLogger {
-}
